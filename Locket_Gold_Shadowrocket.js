9b9b515994e5f9eb4b7dddfd0a7712a9d372704f
N4hzxje <n4hzxje@gmail.com>
2024-08-01 20:13:54 +0700

    Delete Locket_Gold_Shadowrocket.js

--- Locket_Gold_Shadowrocket.js
+++
@@ -1,8 +0,0 @@
-// ========= ID ========= //
-const mapping = {
-  '%E8%BD%A6%E7%A5%A8%E7%A5%A8': ['vip+watch_vip'],
-  'Locket': ['Gold']
-};
-// =========   Phần cố định  ========= // 
-// =========  N4hzxje ========= // 
-var ua=$request.headers["User-Agent"]||$request.headers["user-agent"],obj=JSON.parse($response.body);obj.Attention="This is Locket Gold";var N4hzxje={is_sandbox:!1,ownership_type:"PURCHASED",billing1_issues_detected_at:null,period_type:"normal",expires_date:"2099-12-18T01:04:17Z",grace_period_expires_date:null,unsubscribe_detected_at:null,original_purchase_date:"2008-09-23T01:04:18Z",purchase_date:"2008-09-23T01:04:18Z",store:"app_store"},N4hzxje={grace_period_expires_date:null,purchase_date:"2008-09-23T01:04:17Z",product_identifier:"com.N4hzxje.premium.yearly",expires_date:"2099-12-18T01:04:17Z"};const match=Object.keys(mapping).find(e=>ua.includes(e));if(match){let[e,s]=mapping[match];s?(N4hzxje.product_identifier=s,obj.subscriber.subscriptions[s]=N4hzxje):obj.subscriber.subscriptions["com.N4hzxje.premium.yearly"]=N4hzxje,obj.subscriber.entitlements[e]=N4hzxje}else obj.subscriber.subscriptions["com.N4hzxje.premium.yearly"]=N4hzxje,obj.subscriber.entitlements.pro=N4hzxje;$done({body:JSON.stringify(obj)});
